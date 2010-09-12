4a8c764b1e21c74e65f16c2565db368f600c9501
zyxist <nightstorm@tlen.pl>
2010-09-12 08:51:48 +0200

    Adding the initial facade implementation.

---
+++ lib/Trinity/Web/Service/Facade.php
@@ -0,0 +1,48 @@
+<?php
+/*
+ *  TRINITY FRAMEWORK <http://www.invenzzia.org>
+ *
+ * This file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE. It is also available through
+ * WWW at this URL: <http://www.invenzzia.org/license/new-bsd>
+ *
+ * Copyright (c) Invenzzia Group <http://www.invenzzia.org>
+ * and other contributors. See website for details.
+ */
+namespace Trinity\Web;
+use \Symfony\Component\EventDispatcher\Event;
+use \Trinity\Basement\Service;
+use \Trinity\Web\Facade\Manager as Facade_Manager;
+
+/**
+ * The facade builder.
+ *
+ * @author Tomasz Jędrzejewski
+ * @copyright Invenzzia Group <http://www.invenzzia.org/> and contributors.
+ * @license http://www.invenzzia.org/license/new-bsd New BSD License
+ */
+class Service_Facade extends Service
+{
+	/**
+	 * Preconfigures and initializes the facade manager object.
+	 *
+	 * @return \Trinity\Web\Facade\Manager
+	 */
+	public function getObject()
+	{
+		$manager = new Facade_Manager;
+		$application = \Trinity\Basement\Application::getApplication();
+
+		$application->getEventDispatcher()->connect('controller.web.dispatch.end', function(Event $event) use($manager) {
+			$facade = $manager->getSelectedFacadeClass();
+
+			if($facade !== null)
+			{
+				$facadeBrick = $event['manager']->getBrick($facade);
+				$facadeBrick->dispatch($event['manager']);
+			}
+		});
+
+		return $manager;
+	} // end getObject();
+} // end Service_Facade;
